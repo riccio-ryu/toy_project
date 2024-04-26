894fdde972f2897679645a480141ac960272442b
Yeon-Chang <ters9292@gmail.com>
2024-04-26 17:21:02 +0900

    toys return!!

---
+++ petti/client/src/Components/views/Together/Together.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Ready from '../../utils/Ready';
+
+function Together() {
+  return <div><Ready /></div>;
+}
+
+export default withRouter(Together);
